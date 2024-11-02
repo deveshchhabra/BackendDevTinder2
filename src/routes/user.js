a9bd148116a656cba5bb3187a7a34fccba6f8b14
dev <deveshchhabra@dev.com>
2024-11-02 16:10:57 +0530

    res.json

--- src/routes/user.js
+++ src/routes/user.js
@@ -77,7 +77,7 @@ userRouter.get("/feed",userAuth,async(req,res)=>{
               { _id: { $ne: loggedInUser._id } },
             ],
           }).select(USER_SAFE_DATA).skip(skip).limit(limit);
-        res.send(users);
+        res.json({data:users});
     }catch(err){
         res.status(400).json({message:err.message});
     }
